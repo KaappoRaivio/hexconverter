568de4175dbce92abc19e94b42b1edf08594972a
NoSocksForYou <kaappo.raivio@gmail.com>
2018-09-11 11:00:24 +0300

    copytoclipboard done

--- app/src/main/java/kaappo/hexconverter/MainActivity.java
+++ app/src/main/java/kaappo/hexconverter/MainActivity.java
@@ -1,17 +1,54 @@
 package kaappo.hexconverter;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public static String getId(View view) {
         if (view.getId() == 0xffffffff) return "no-id";
         else return view.getResources().getResourceName(view.getId());
+    }
+
+    public void onCopyButtonPress (View v) {
+        EditText editText;
+
+        switch (v.getId()) {
+
+
+            case R.id.binary_copy:
+                editText = (EditText) findViewById(R.id.binary);
+                break;
+            case R.id.hexadecimal_copy:
+                editText = (EditText) findViewById(R.id.hexadecimal);
+                break;
+            case R.id.octal_copy:
+                editText = (EditText) findViewById(R.id.octal);
+                break;
+            case R.id.decimal_copy:
+                editText = (EditText) findViewById(R.id.decimal);
+                break;
+
+            default:
+                System.out.println("ASDASDASDASDASd");
+                editText = null;
+                break;
+
+        }
+        String textToCopy = editText.getText().toString();
+
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("label", textToCopy);
+        clipboard.setPrimaryClip(clip);
+        toast("Copied to clipboard");
+
     }
 
     @Override
@@ -24,22 +61,9 @@ public class MainActivity extends AppCompatActivity {
             System.out.println(e.toString());
         }
 
-//        EditText hex = (EditText) findViewById(R.id.hexadecimal);
-//        hex.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                System.out.println(s + "," + start + "," + before + "," + count);
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//
-//            }
-//        });
+    }
+
+    public void toast (String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
 }
